8bdee18203d21cfdc0bb92c53d74b49f70e0cc03
--replace-all <--replace-all>
2016-03-12 22:30:38 +0200

    magic methods

---
+++ IdeaProjects/Magic Methods/Cars.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Roger Nkosi
+ * Date: 3/12/16
+ * Time: 9:39 PM
+ */
+class Cars
+{
+    public function limou(){
+        echo "This car is long";
+    }
+}
+
+$car = new Cars();
+$car->limou();
